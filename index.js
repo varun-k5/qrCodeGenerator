a0cb5e1c20f586c175c1f5b3e1d735826d5480aa
varun kaushik <varun0509kaushik@gmail.com>
2024-08-30 22:49:47 +0530

    Download

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express=require('express')
 const QRcode=require('qrcode')
 const validUrl=require('valid-url')
+const path = require('path')
+const fs = require('fs')
 
 const app=express()
 const port=process.env.PORT||3000
@@ -14,7 +16,29 @@ app.get('/generateQR', async (req,res)=>{
         }
       
         const qrCodeImage=await QRcode.toDataURL(url)
-        res.send(`<img src="${qrCodeImage}" alt="QR code"/>`)
+
+        const tempFilePath=path.join(__dirname,'qrcode.png')
+        await QRcode.toFile(tempFilePath,url)
+
+        res.send(`
+            <img src="${qrCodeImage}" alt="QR code"/>
+            <br/>
+            <a href="/downloadQR" download="qrcode.png">
+            <button>Download</button>
+            </a>
+        `)
+
+        //Route to serve the QR code image for download
+        app.get('/downloadQR',(req,res)=>{
+            res.download(tempFilePath,'qrcode.png',(err)=>{
+                if(err){
+                    console.log(err)
+                    res.status(500).send('Error downloading this file')
+                }
+                //Delete the file after download
+                fs.unlinkSync(tempFilePath)
+            })
+        })
     }
     catch(e){
         console.log(e)
@@ -25,3 +49,10 @@ app.get('/generateQR', async (req,res)=>{
 app.listen(port,()=>{
     console.log(`Server is set up on port ${port}.`)
 })
+
+/*
+Usage:
+Temporary File Creation: You create a temporary file(on server) to generate and serve a QR code.
+File Download: The client downloads the file.
+File Deletion: After the download, the server removes the file to keep the system clean.
+*/
